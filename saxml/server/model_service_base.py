86779b740261f639d2fb5ac78447b69061448c9a
Aleksey Vlasenko <vlasenkoalexey@gmail.com>
2024-01-12 22:45:23 +0000

    fixing build for Python 3.10 StrEnum is not supported

--- saxml/server/model_service_base.py
+++ saxml/server/model_service_base.py
@@ -30,6 +30,7 @@ from typing import Any, Awaitable, Callable, Deque, Dict, List, Mapping, Optiona
 import uuid
 
 from absl import logging
+from enum import Enum
 import grpc
 from grpc_reflection.v1alpha import reflection
 import numpy as np
@@ -97,7 +98,7 @@ def _maybe_all_cancelled(
 
 
 @enum.unique
-class MethodName(enum.StrEnum):
+class MethodName(str, Enum):
   """Sax method types."""
 
   # Sax internal methods
